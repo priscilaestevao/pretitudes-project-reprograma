d80aeb333330b8bb07d2d7b2a397fad114a0400d
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-11-28 18:02:48 -0300

    connection configuration with the mongo atlas database

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
-const port = 8080;
+const port = process.env.PORT;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.info(`Listening on port ${port}`);
 });
--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
+require("dotenv-safe").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/dbBlackCandidates", {
+mongoose.connect(`${process.env.MONGODB_URL}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-let db = mongoose.connection;
+let db = mongoose.connection; 
 db.on("error", console.log.bind(console, "Connection error!"));
 db.once("open", () => {
   console.log("Sucessfully connected!");
