e7227a61c4d0eb895fd74f319916906b5ce85353
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-11-28 21:03:40 -0300

    route creation candidate removal

--- src/controllers/candidatesController.js
+++ src/controllers/candidatesController.js
@@ -26,23 +26,32 @@ const updateRegistration = (req, res) => {
   const id = req.params.id;
   const updateCandidate = req.body;
 
-  candidatesModel.findByIdAndUpdate(
-    id,
-    updateCandidate,
-    (err, candidate) => {
-      if (err) {
-        return res.status(424).send({ message: err.message });
-      }
-      else if (candidate) {
-        return res.status(200).send("Updated successfully!");
-      }
-      res.status(404).send("Not found!")
+  candidatesModel.findByIdAndUpdate(id, updateCandidate, (err, candidate) => {
+    if (err) {
+      return res.status(424).send({ message: err.message });
+    } else if (candidate) {
+      return res.status(200).send("Updated successfully!");
     }
-  );
+    res.status(404).send("Register not found!");
+  });
+};
+
+const removeCandidate = (req, res) => {
+  const id = req.params.id;
+
+  candidatesModel.findByIdAndDelete(id, (err, candidate) => {
+    if (err) {
+      return res.status(424).send({ message: err.message });
+    } else if (candidate) {
+      return res.status(200).send("Succssfully removed!");
+    }
+    res.status(404).send("Register not found!")
+  });
 };
 
 module.exports = {
   allCandidates,
   createCandidate,
   updateRegistration,
+  removeCandidate
 };
