13777106bf536ef3af8eb3bb3888e07a1c5e5ac1
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-12-06 10:13:56 -0300

    add new attribute in Schema

--- src/models/black-candidates.js
+++ src/models/black-candidates.js
@@ -3,7 +3,12 @@ const candidatesSchema = new mongoose.Schema(
   {
     _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
     nome: { type: String },
+    nomeSocial: { type: String },
     dataNascimento: { type: String },
+    genero: { type: String },
+    racaCor: { type: String },
+    grauInstrucao: { type: String },
+    ocupacao: { type: String },
     telefone: { type: String },
     email: { type: String },
     cidade: { type: String },
