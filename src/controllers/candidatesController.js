fdb68e49df04fa7ca46b73704f8bcea58663fcd3
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-12-06 19:02:01 -0300

    route creation for candidate by id

--- src/controllers/candidatesController.js
+++ src/controllers/candidatesController.js
@@ -10,6 +10,35 @@ const allCandidates = (req, res) => {
   });
 };
 
+const candidatesById = (req, res) => {
+  const id = req.params.id;
+  candidatesModel.findById(id, (err, candidate) => {
+    if (err) {
+      res.status(424).send({ message: err.message });
+    } else if (candidate) {
+      return res.status(200).send(candidate);
+    }
+    res.status(404).send("Candidate not found!");
+  });
+};
+
+const candidatesByCity = (req, res) => {
+  const city = req.params.cidade;
+  candidates.findByCity(city, (err, candidatesRMRList) => {
+    if (err) {
+      return res.status(424).send({ message: err.message });
+    } else if (candidatesRMRList) {
+      return res.status(200).send({
+        nome: nomeSocial,
+        movimento: movimentoSocial,
+        candidatura: tipoCandidatura,
+        partido: partido,
+      });
+    }
+    res.status(404).send("City not found!");
+  });
+};
+
 const createCandidate = (req, res) => {
   const newCandidate = new candidatesModel(req.body);
   newCandidate.save((err) => {
@@ -40,18 +69,19 @@ const updateRegistration = (req, res) => {
 const removeCandidateByEmptyPopularMovement = (req, res) => {
   const params = req.query;
   candidatesModel.deleteMany(params, (err, popularMovement) => {
-      if (err) {
-        return res.status(424).send({ message: err.message });
-      } else if (popularMovement) {
-        return res.status(200).send("Successfully removed!");
-      }
-      res.status(404).send("Register not found!");
+    if (err) {
+      return res.status(424).send({ message: err.message });
+    } else if (popularMovement) {
+      return res.status(200).send("Successfully removed!");
     }
-  );
+    res.status(404).send("Register not found!");
+  });
 };
 
 module.exports = {
   allCandidates,
+  candidatesById,
+  candidatesByCity,
   createCandidate,
   updateRegistration,
   removeCandidateByEmptyPopularMovement,
--- src/routes/candidatesRoute.js
+++ src/routes/candidatesRoute.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const controller = require("../controllers/candidatesController");
 
 router.get("/", controller.allCandidates);
+router.get("/:id", controller.candidatesById);
+router.get("/lista/:cidade", controller.candidatesByCity);
 router.post("/", controller.createCandidate);
 router.put("/:id", controller.updateRegistration);
 router.delete("/participacao", controller.removeCandidateByEmptyPopularMovement);
