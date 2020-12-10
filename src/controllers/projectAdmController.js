e1d89854e622e88abd8ed8af6ac83cea0024c560
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-12-09 23:37:03 -0300

    creation of authenticated requisition

--- src/controllers/projectAdmController.js
+++ src/controllers/projectAdmController.js
@@ -1,8 +1,33 @@
+require("dotenv-safe").config();
 const projectAdmModel = require("../models/projectAdm");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const SECRET = process.env.SECRET;
 
-const allAdmnistrators = (req, res) => {};
+const allAdmnistrators = (req, res) => {
+  projectAdmModel.find((err, administrators) => {
+    if (err) {
+      return res.status(424).send({ message: err.message });
+    }
+    res.status(200).send(administrators);
+  });
+};
 
-const createAdministrator = (req, res) => {};
+const createAdministrator = (req, res) => {
+  const encryptedPassword = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = encryptedPassword;
+  const newAdm = new projectAdmModel(req.body);
+
+  newAdm.save((err) => {
+    if (err) {
+      return res.status(424).send({ message: err.message });
+    }
+    res.status(201).send({
+      message: "Administrator successfully registered!",
+      administrator: newAdm,
+    });
+  });
+};
 
 const login = (req, res) => {};
 
--- src/routes/projectAdmRoute.js
+++ src/routes/projectAdmRoute.js
@@ -1,8 +1,29 @@
+require("dotenv-safe").config();
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/projectAdmController");
 
-router.get("/", controller.allAdmnistrators);
+const authAdm = (req, res, next) => {
+  const authHeader = req.get("authorization");
+  if (!authHeader) {
+    return res.status(401).send("You need to login!");
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, SECRET, (err) => {
+    if (err) {
+      return res.status(403).send("Invalid token!");
+    }
+    projectAdmModel.find((err, administrators) => {
+      if (err) {
+        return res.status(424).send({ message: err.message });
+      }
+      res.status(200).send(administrators);
+    });
+  });
+  next();
+};
+
+router.get("/", authAdm ,controller.allAdmnistrators);
 router.post("/", controller.createAdministrator);
 router.post("/login", controller.login);
 router.put("/:id", controller.updateAdministrator);
